2d1827054af2720a9ec0491f2ad5ab603c3d612e
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-02-15 15:54:28 +0530

    Added Login Card Outline

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,10 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from "../../common/header/Header.js";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import "./Login.css";
 
 class Login extends Component {
     render() {
-        return(
-            <Header headerTitle="Image Viewer"/>
+        return (
+            <div>
+                <Header headerTitle="Image Viewer" />
+                <div className="loginCardContainer">
+                    <Card className="loginCard">
+                        <CardContent>
+                            <h4>LOGIN</h4>
+                        </CardContent>
+                    </Card>
+                </div>
+
+            </div>
         )
     }
 }
