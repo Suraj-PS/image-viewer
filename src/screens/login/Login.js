156578ca155d420eea13033fd73924e48af4b4b8
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-02-15 23:28:05 +0530

    Added Username and Password Validation

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 import "./Login.css";
 import Header from "../../common/header/Header.js";
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+            validationTextClass: "displayNone"
+        }
+    }
+
+    usernameChangeHandler = event => {
+        this.setState({ username: event.target.value })
+    }
+
+    passwordChangeHandler = event => {
+        this.setState({ password: event.target.value })
+    }
+
+    loginClickHandler = () => {
+        let correctUserName = "Suraj";
+        let correctPassword = "upgrad123";
+
+        if (this.state.username !== correctUserName) {
+            this.setState({ validationTextClass: "displayValidation" });
+            return;
+        }
+
+        if (this.state.password !== correctPassword) {
+            this.setState({ validationTextClass: "displayValidation" });
+            return;
+        }
+
+        this.setState({ validationTextClass: "displayNone" });
+    }
+
+
     render() {
         return (
             <div>
@@ -17,16 +53,19 @@ class Login extends Component {
                     <Card className="loginCard">
                         LOGIN
                         <div className="cardContent">
-                            <FormControl required="true">
+                            <FormControl required={true}>
                                 <InputLabel htmlFor="username">Username</InputLabel>
-                                <Input id="username" placeholder="Username" />
+                                <Input id="username" placeholder="Username" onChange={this.usernameChangeHandler} />
                             </FormControl>
-                            <FormControl required="true">
+                            <FormControl required={true}>
                                 <InputLabel htmlFor="passowrd">Password</InputLabel>
-                                <Input id="password" placeholder="Password" />
+                                <Input id="password" placeholder="Password" onChange={this.passwordChangeHandler} />
                             </FormControl>
+                            <FormHelperText className={this.state.validationTextClass}>
+                                <span className="displayRed">Incorrect username and/or password</span>
+                            </FormHelperText>
                         </div>
-                        <Button variant="contained" size="small" color="primary">LOGIN</Button>
+                        <Button variant="contained" size="small" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
                     </Card>
                 </div>
             </div>
