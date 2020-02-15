cf7b3fa9dc464f4e5f9ae8d0b192c9777649c79e
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-02-15 17:00:31 +0530

    Added Login Input Fields and associated Styles

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import "./Login.css";
 import Header from "../../common/header/Header.js";
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import "./Login.css";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 class Login extends Component {
     render() {
@@ -11,12 +15,20 @@ class Login extends Component {
                 <Header headerTitle="Image Viewer" />
                 <div className="loginCardContainer">
                     <Card className="loginCard">
-                        <CardContent>
-                            <h4>LOGIN</h4>
-                        </CardContent>
+                        LOGIN
+                        <div className="cardContent">
+                            <FormControl required="true">
+                                <InputLabel htmlFor="username">Username</InputLabel>
+                                <Input id="username" placeholder="Username" />
+                            </FormControl>
+                            <FormControl required="true">
+                                <InputLabel htmlFor="passowrd">Password</InputLabel>
+                                <Input id="password" placeholder="Password" />
+                            </FormControl>
+                        </div>
+                        <Button variant="contained" size="small" color="primary">LOGIN</Button>
                     </Card>
                 </div>
-
             </div>
         )
     }
