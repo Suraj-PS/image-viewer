1fa0eb47c3f0a3613b83fb3430f350590b38bb48
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-02-15 13:45:23 +0530

    Added Login JS and CSS Files

--- src/ImageViewer.js
+++ src/ImageViewer.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Login from "./screens/login/Login.js";
 
 class ImageViewer extends Component {
+    render() {
+        return(
+            <Login />
+        )
+    }
 }
+
+export default ImageViewer;
